967bc14d0acef51f4ead9101b10263ec9e34f5b8
Evan Miller <evan.s.miller3@gmail.com>
2024-05-22 11:56:08 -0400

    updated App.jsx

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -6,25 +6,69 @@ import DuckPage from './components/DuckPage'
 import Cart from './components/Cart'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([])
+  useEffect(() => {
+    const fetchData = async ()=> {
+      try {
+        const response = await fetch(import.meta.env.VITE_DUCKS_API)
+        if(!response.ok) {
+          throw new Error('Data could not be fetched')
+        }
+        const json_response = await response.json()
+        setData(json_response)
+      } catch(error) {
+        console.log('error fetching ducks:', error)
+      }
+    }
+    fetchData()
+  }, [page])
+  
+  const handleDelete = async (duck_id) => {
+     try{
+      const response = await fetch(`${import.meta.env.VITE_DUCK_API}/${duck_id}`, {
+        method:'DELETE',
+      })
+      if(!response.ok) {
+        throw new Error('Duck could not be deleted')
+      }
+  
+     } catch (error) {
+      console.log('error deleting duck')
+     }
+  }
 
   return (
     <>
-      <div>
-       
-      </div>
-      <h1>Duck Store</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Router>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Rubber Duck Store</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">
+                    Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/cart">
+                    Cart
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="checkout">
+                  Checkout
+                </Link>
+              </li>
+            </ul>
+            <Search setData={setData} />
+          </div>
+        </div>
+      </nav>
+      </Router>
     </>
   )
 }
