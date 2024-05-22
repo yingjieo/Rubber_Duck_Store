499dbcaa574ebf6624a84a97dd1a034f96db799c
Evan Miller <evan.s.miller3@gmail.com>
2024-05-22 11:17:56 -0400

    added ite

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import {useState, useEffect} from react
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import DuckPage from './components/DuckPage'
+import DuckPage from '../../react/src/components/DuckPage'
 
 function App() {
     const [data, setData] = useState([])
+    useEffect(()=> {
+        try{
+            const response = await fetch(import.)
+        }
+    })
     return (
         <>
         
