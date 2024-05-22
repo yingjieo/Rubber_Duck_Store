edea6d4f080e2d518bb9b0246fe829e9ba62d47e
Evan Miller <evan.s.miller3@gmail.com>
2024-05-22 16:49:48 -0400

    next and prev buttons

--- react/src/components/Home.jsx
+++ react/src/components/Home.jsx
@@ -1,14 +1,38 @@
+import React, {useState} from 'react'
 import DuckPage from "./DuckPage"
 
 const Home = (props) => {
+    const [currentPage, setCurrentPage] = useState(1)
+    const ducksPerPage = 9
+
+    const indexOfLastDuck = currentPage * ducksPerPage
+    const indexOfFirstDuck = indexOfLastDuck - ducksPerPage
+    const currentDucks = props.data.slice(indexOfFirstDuck, indexOfLastDuck)
+
+    const nextPage = () => {
+        setCurrentPage(currentPage + 1)
+    }
+    const prevPage = () => {
+        setCurrentPage(currentPage - 1)
+    }
     return(
-        <div className="card-container" style={{ display: 'flex', flexWrap: 'wrap', gap: '20px' }}>
-            {
-                props.data.map((ducks) => (
-                    <DuckPage key={ducks._id} data={ducks} />
-                ))
-            }
-        </div>
+        <>
+            <div className="card-container" style={{ display: 'flex', flexWrap: 'wrap', gap: '20px' }}>
+                {
+                    currentDucks.map((ducks) => (
+                        <DuckPage key={ducks._id} data={ducks} />
+                    ))
+                }
+            </div>
+            <div style={{ textAlign: 'center'}}>
+                {currentPage > 1 && (
+                    <button onClick={prevPage}>Previous</button>
+                )}
+                {indexOfLastDuck< props.data.length && (
+                    <button onClick={nextPage}>Next</button>
+                )}
+            </div>
+        </>
     )
 }
 export default Home
