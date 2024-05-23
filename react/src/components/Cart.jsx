8cc3ada7d574b5ecd8c33a34a9e39878e334b37e
EvanSterlingMiller <evan.s.miller3@gmail.com>
2024-05-23 06:46:24 -0400

    cart

--- react/src/components/Cart.jsx
+++ react/src/components/Cart.jsx
@@ -1,14 +1,44 @@
-import React from 'react'
-import CartContent from './CartContent'
+import React, { useState, useEffect } from 'react';
+import CartContent from './CartContent';
+import Cart from './Cart';
 
-function Cart(props)  {
-    const {cart, addToCart} = useContent(CartContent)
-    return(
-        <div className='container cart'>
-            <div>Size: {props.data?.size}</div>
-        </div>
-        
-        
-    )
+function Cart(props) {
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    // Retrieve cart data when the component mounts
+    retrieveCart();
+  }, []);
+
+  const retrieveCart = async () => {
+    // Call the retrieve method of your CartContent to get the current cart data
+    const response = await CartContent.retrieve();
+    if (response.cart) {
+      setCart(response.cart);
+    }
+  };
+
+  const handleRemoveFromCart = async (duck_id) => {
+    // Call the remove method of your CartContent to remove the duck from the cart
+    const response = await CartContent.remove(duck_id);
+    if (response.cart) {
+      setCart(response.cart);
+    }
+  };
+
+  return (
+    <div className="container cart">
+      <h2>Cart</h2>
+      <div className="cart-items">
+        {cart.map((item) => (
+          <div key={item.duck_id} className="cart-item">
+            <div>Size: {item.size}</div>
+            <button onClick={() => handleRemoveFromCart(item.duck_id)}>Remove</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
-export default Cart
+
+export default Cart;
