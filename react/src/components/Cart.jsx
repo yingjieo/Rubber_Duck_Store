e47208eb129d531a2becae498fb714d8829e66a2
Alex <alex@webage.com>
2024-05-23 19:25:16 -0400

    Updated visuals of ducks in cart

--- react/src/components/Cart.jsx
+++ react/src/components/Cart.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import {Link} from 'react-router-dom'
 import Checkout from './Checkout'
+import duck_image from '../../../images/rubber_duck.png';
 
 
 function Cart({cart, setCart}) {
@@ -21,14 +22,13 @@ function Cart({cart, setCart}) {
       <div className="cart-items">
         {cart.map((item) => (
           <div key={item.duck_id} className="cart-item">
-            
-            <h5 className="card-title">Duck Details</h5>
+            <section id='img'>
+                <img src={duck_image} />
+            </section>
+            <h5 className="card-title">{item.name}</h5>
             <div className="card-text">Color: {item.color}</div>
             <div className="card-text">Size: {item.size} </div>
-            <div className="card-text">Material: {item.material} </div>
-            <div className="card-text">Animal: {item.animal}</div>
-            <div className="card-text">Pattern: {item.pattern}</div>
-            <div className="card-text">Theme: {item.theme}</div>
+            <div className="card-text">Popularity: {"⭐".repeat(item.popularity)}</div>
             <div className="card-text">Price: ${item.price}</div>
             <button onClick = {() => removeFromCart(item)}>Remove from Cart</button>
             
